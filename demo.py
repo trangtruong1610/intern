fd6dfb20d9a2666e5c44f0c46874bc27eb17988d
Trang <trangtruong16101993@gmail.com>
2019-10-09 12:58:32 +0700

    fix demo

--- demo.py
+++ demo.py
@@ -3,13 +3,13 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import  declarative_base
 import sqlalchemy as db
 
-class database:
-    engine = db.create_engine('postgresql+psycopg2://postgres:trangtruong@localhost:5432/ggbenefits')
-    Session = sessionmaker(bind=engine)
-    session = Session()
+engine = db.create_engine('postgresql+psycopg2://postgres:trangtruong@localhost:5432/ggbenefits')
+Session = sessionmaker(bind=engine)
+session = Session()
 
 Base = declarative_base()
-class customer(Base):
+
+class Customer(Base):
     __tablename__ = 'clinics'
     name = Column(String, primary_key=True)
     address = Column(String)
@@ -17,5 +17,5 @@ class customer(Base):
     lat = Column(Float)
     lng = Column(Float)
 
-for i in database.session.query(customer):
+for i in session.query(Customer):
     print(f'{i.name, i.address, i.phone, i.lat, i.lng}')
